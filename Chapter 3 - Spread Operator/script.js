e0c5513ba4179d59b3d010c6080ee00893fbb399
Chhiring Lama <lamachhiring103@gmail.com>
2024-03-05 11:51:13 +0545

    Chapter 3 Update

--- Chapter 3 - Spread Operator/script.js
+++ Chapter 3 - Spread Operator/script.js
@@ -1,1 +1,94 @@
 "use strict";
+
+//A Quick Revision on Chapter 2
+//Conside an Example where you are viewing a hotels' website and you logged in with your username
+var appUser="Chhiring Lama";
+
+const hotel = {
+  hotelName: "KTM valley View",
+  starRating: 5,
+  rooms: [1,2,3,4,5,6,7,8,9,10],
+  cuisine: ["Italian", "Mexican", "Indian"],
+  bookRoom:function({appUser:userName,roomNo=1,selectedDish}){
+    console.log(`${userName} booked room no ${this.rooms[roomNo]} with meal plan ${this.cuisine[selectedDish]}`);
+  },
+  orderDetail:function(detail_1,detail_2,detail_3){
+    console.log(`${detail_1},${detail_2},${detail_3}`);
+  }
+};
+
+let { hotelName, rooms: roomsAvailable = 10, cuisine: dishRange = [] } = hotel;
+
+console.log(hotelName, roomsAvailable, dishRange);
+
+hotel.bookRoom({
+  appUser:appUser,
+  roomNo:4,
+  selectedDish:1,
+});
+
+//-----------------
+
+// Spread Operator 
+// A unary Operator that spreads all the elements of an iterables and object. 
+
+
+// Helpful for,
+// 1, Joining/merge two or more arrays,
+// 2, Copying arrays
+// 3, spreading elements of a iterables (array,strings,maps,sets but NOT!! OBJECT !!) and Object
+
+//Array Spread
+//This explains, joining and Copying;
+const newMenu=["Nepali","Chinese","American"]
+const cuisineUpdated=[...hotel.cuisine,...newMenu];
+console.log(...cuisineUpdated);
+
+
+//String Spread
+let userName="Chhiring";
+console.log(...userName );
+
+//Each element is passed individually into a function
+function printArray(...elements){
+    console.log(elements);
+}
+printArray(...newMenu)
+
+
+//Function to Intake multiple arguments
+// const details=[prompt("Please\' Enter Detail 1"),prompt("Enter Detail 2"),prompt("Detail 3")];
+
+//Basic Way to pass the value to the function 
+// hotel.orderDetail(details[0],details[1],details[2]);
+
+//With Spread we can simply pass the array with spread operator
+// hotel.orderDetail(...details);
+
+
+
+
+
+
+//Spreading With Object
+
+//Basic way of Object Copy
+let testObject={};
+Object.assign(testObject,hotel);
+console.log(testObject);
+console.log(hotel);
+
+//Both are true copy and not pointing to same object property like 
+//testObject=hotel;
+//Proof:
+testObject.hotelName="Kira Sen";
+
+console.log(testObject);
+console.log(hotel);
+
+
+//Spreading Technique
+let hotelBranch ={branchEstDate:"2022",...hotel,branchLocation:"Thapathali"};
+hotelBranch.hotelName="KTM Second View";
+console.log(hotelBranch);
+console.log(hotel);
